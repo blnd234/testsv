b79eb33553df843484c91e4950b4c5ee012d1284
blnd234 <blndbl4ne.i1@gmail.com>
2025-05-24 02:56:07 +0300

    ll

--- resources/views/github.blade.php
+++ resources/views/github.blade.php
@@ -118,7 +118,7 @@
 <div class="container">
     <div class="profile-card">
         <div class="profile-image">
-            <img src="{{ asset('assets/image/Me.JPG') }}">
+            <img src="{{ asset('assets/image/me.JPG') }}">
         </div>
 
         <div class="username">
